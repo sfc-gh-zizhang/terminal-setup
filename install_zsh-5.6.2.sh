a01f4edae639524d87a71b5e36e72900e97ac2b6
Felix Zhang <felix.zhang@snowflake.com>
2024-04-11 09:24:08 +0000

    Add more condition

--- install_zsh-5.6.2.sh
+++ install_zsh-5.6.2.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-sudo -E yum groupinstall "Development tools"
-sudo -E yum install ncurses-devel
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+USERNAME=$1
+sudo -E yum -y groupinstall "Development tools"
+sudo -E yum -y install ncurses-devel
 wget https://sourceforge.net/projects/zsh/files/zsh/5.6.2/zsh-5.6.2.tar.xz/download
 tar -xvJf download
 cd zsh-5.6.2
 ./configure
-sudo make
-sudo make install
+make
+make install
 echo "/usr/local/bin/zsh-5.6.2" >> /etc/shells
 chsh -s /usr/local/bin/zsh-5.6.2
-chsh -s /usr/local/bin/zsh-5.6.2 $USER
+chsh -s /usr/local/bin/zsh-5.6.2 $USERNAME
+
--- set_terminal_all_in_one.sh
+++ set_terminal_all_in_one.sh
@@ -1,19 +1,59 @@
 #!/bin/bash
 
-bash ./install_zsh-5.6.2.sh
-exec zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-exec zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo "ZSH_THEME="powerlevel10k/powerlevel10k"" >> ~/.zshrc
-exec zsh
-p10k configure
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo '
+# Check if the current shell is not /usr/local/bin/zsh-5.6.2
+if [[ "$(readlink /proc/$$/exe)" != "/usr/local/bin/zsh-5.6.2" ]]; then
+    echo "Current shell is not /usr/local/bin/zsh-5.6.2, attempting to install and switch..."
+    # Attempt to run the installation script for zsh-5.6.2
+    sudo bash ./install_zsh-5.6.2.sh $USER
+fi
+
+# Install Oh My Zsh non-interactively (without changing the default shell)
+RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Clone the Powerlevel10k theme if it doesn't exist
+if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]]; then
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+	# Check and update ZSH_THEME
+	if grep -q '^ZSH_THEME=".*"' "$ZSHRC"; then
+	    # ZSH_THEME exists, change it to powerlevel10k/powerlevel10k
+	    sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$ZSHRC"
+	else
+	    # ZSH_THEME does not exist, add it
+	    echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> "$ZSHRC"
+	fi
+fi
+
+# Clone zsh-autosuggestions plugin if it doesn't exist
+if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+fi
+
+# Clone zsh-syntax-highlighting plugin if it doesn't exist
+if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+fi
+
+# Function to check and add plugin if not exists
+add_plugin_if_not_exists() {
+    local plugin=$1
+    if ! grep -q "$plugin" "$ZSHRC"; then
+        # Plugin not found, add it
+        sed -i "/^plugins=(/s/)$/ $plugin)/" "$ZSHRC"
+    fi
+}
+
+# Ensure plugins array exists
+if grep -q '^plugins=(.*)' "$ZSHRC"; then
+    # Check each plugin and add if it doesn't exist
+    add_plugin_if_not_exists "zsh-syntax-highlighting"
+    add_plugin_if_not_exists "zsh-autosuggestions"
+else
+    # plugins array doesn't exist, add it with the required plugins
+    echo '
 plugins=( 
     zsh-syntax-highlighting
     zsh-autosuggestions
 )
-' >> ~/.zshrc
-exec zsh
+' >> "$ZSHRC"
+fi
+
